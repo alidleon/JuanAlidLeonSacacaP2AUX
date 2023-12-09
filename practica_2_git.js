944d790a8fb37644ff3af1e95f5bc954c831a27a
alidleon <alidleon555@gmail.com>
2023-12-08 23:34:23 -0400

    Objects V2

--- practica_2_git.js
+++ practica_2_git.js
@@ -8,3 +8,23 @@ const suma=(a,b)=>{
 }
 
 console.log (suma(5,10));
+
+
+// Objects
+let cup= {
+    color:"rojo",
+    desing:"san valentin",
+    weight:"5.1 kg",
+    material: "marmol"
+}
+
+
+
+const Student= {
+    firtName: "alid",
+    lastName: "leon",
+    age: 26,
+    hubby: "dormir"
+}
+
+console.log(Student);
